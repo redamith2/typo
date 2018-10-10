ad6779e5bfe7a9130984c60d6c3f9c360f47a666
EC2 Default User <ec2-user@ip-172-31-29-83.us-east-2.compute.internal>
2018-10-10 22:47:38 +0000

    added a scenario where you can edit categories

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -33,6 +33,13 @@ describe Admin::CategoriesController do
       assigns(:categories).should_not be_nil
     end
   end
+  
+  it 'should create a new category' do
+    post :edit, :category =>{:name => "random1", :keywords => "random2", :permalink => "random3", :description => "random4"}
+    assert_response :redirect, :action => "index"
+    assert_not_nil assigns(:category)
+    expect(flash[:notice]).to eq("Category was successfully saved.")
+  end
 
   it "test_update" do
     post :edit, :id => Factory(:category).id
